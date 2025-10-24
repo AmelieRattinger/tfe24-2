ca9660b9fb1959ef97088451dab6c3c92c5827bc
AmelieRattinger <rattinger.a-it24@it.dhbw-ravensburg.de>
2025-10-24 12:10:37 +0200

    feat: add exercise number two_2

--- exercise-004/point.cpp
+++ exercise-004/point.cpp
@@ -1,11 +1,22 @@
 #include "point.hpp"
+#include <fmt/core.h>
 
-Point::Point()
-{
+// Standardkonstruktor
+Point::Point() : m_x(0), m_y(0) {}
 
+// Konstruktor mit Parametern
+Point::Point(double x, double y) : m_x(x), m_y(y) {}
+
+// Destruktor
+Point::~Point() {}
+
+// Methode zum Verschieben
+void Point::move(double dx, double dy) {
+    m_x += dx;
+    m_y += dy;
 }
 
-Point::Point(double x, double y) {
-    m_x = x;
-    m_y = y;    
+// Methode zur Ausgabe
+void Point::print() const {
+    fmt::print("({}, {})\n", m_x, m_y);
 }
--- exercise-004/point.hpp
+++ exercise-004/point.hpp
@@ -1,16 +1,20 @@
 #ifndef POINT_HPP
 #define POINT_HPP
 
+
+#include <fmt/core.h> // fmt-Bibliothek für die Ausgabe
+
 class Point {
-    public:
-        Point();
-        Point(double x, double y);
-        ~Point();
-    private:
-        double m_x;
-        double m_y;
+public:
+    Point();                         // Standardkonstruktor
+    Point(double x, double y);       // Konstruktor mit Parametern
+    ~Point();                        // Destruktor
 
-
+    void move(double dx, double dy); // Methode zum Verschieben
+    void print() const;              // Methode zur Ausgabe
+private:
+    double m_x;
+    double m_y;
 };
 
-#endif // POINT_HPP
+#endif // POINT_HP
